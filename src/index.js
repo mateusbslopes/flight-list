5cd9cd39f91e9a172ae9c2aa86f4ff2d421251a9
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-01-09 17:35:24 -0300

    refactor: Configure webpack to generate app

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import FlightsService from "./Service/Flights";
-import FlightColumn from "./components/FlightColumn";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.flightsService = new FlightsService();
-    this.state = { flights: null };
-  }
-
-  componentDidMount() {
-    this.flightsService
-      .getFlights()
-      .then(flights => this.setState({ flights: flights }));
-  }
-
   render() {
-    return <div>{this.state.flights && <FlightColumn />}</div>;
+    return <div>Teste</div>;
   }
 }
 
