e525984858d24e2735c71e65ca75671558301964
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-01-10 10:41:21 -0300

    style: Add font-family

--- src/atoms/Info/style.js
+++ src/atoms/Info/style.js
@@ -1,6 +1,7 @@
 const style = () => `
     color: #c8ccd4;
     font-size: 10px;
+    font-family: Montserrat, arial, sans-serif;
 `;
 
 export default style;
--- src/atoms/Text/style.js
+++ src/atoms/Text/style.js
@@ -1,7 +1,8 @@
 const style = () => `
     justify-content: center;
+    font-family: Montserrat, arial, sans-serif;
     color: rgb(135, 147, 149);
-    font-weiht: 600;
+    font-weight: 600;
     font-size: 12px;
 `;
 
