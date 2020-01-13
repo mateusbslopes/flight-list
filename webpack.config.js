d4666c32e97ed203269efeeaca074c290e75f69a
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-01-13 13:06:47 -0300

    feat: Add error message to proper configure environment

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = env => {
+  if (!env.API_TOKEN)
+    throw new Error(
+      "Configure the API_TOKEN env var. See README.md to know how to do it"
+    );
   return {
     entry: "./src/",
 
