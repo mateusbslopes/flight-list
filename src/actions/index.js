e06e4458d482f50884303f678eb0bc974a3f810f
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-01-22 14:11:22 -0300

    feat: Add redux with async actions

---
+++ src/actions/index.js
@@ -0,0 +1,34 @@
+// Action types
+export const ADD_FLIGHTS = "ADD_FLIGHTS";
+export const SET_AIRPORTS = "SET_AIRPORTS";
+export const SET_DISPLAYED_FLIGHTS = "SET_DISPLAYED_FLIGHTS";
+export const GET_AIRPORTS = "GET_AIRPORTS";
+export const GET_AIRPORTS_SUCCESS = "GET_AIRPORTS_SUCCESS";
+
+// Project conts
+export const DisplayableFlights = {
+  INBOUND: "inbound",
+  OUTBOUND: "outbound"
+};
+
+// Actions
+export const getAirports = () => async dispach => {
+  dispach({ type: GET_AIRPORTS });
+  let response = fetchAirports();
+  dispach(airportsSuccess(response.data));
+};
+
+export const airportsSuccess = airports => ({
+  type: GET_AIRPORTS_SUCCESS,
+  payload: { airports }
+});
+
+export const setAirports = airports => ({
+  type: SET_AIRPORTS,
+  payload: { airports }
+});
+
+export const addFlights = flights => ({
+  type: ADD_FLIGHTS,
+  payload: { flights }
+});
---
+++ src/reducers/index.js
@@ -0,0 +1,48 @@
+import { createStore } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { ADD_FLIGHTS, SET_AIRPORTS, SET_DISPLAYED_FLIGHTS } from "../actions";
+
+const initialState = {
+  flights: null,
+  airports: null,
+  searchIntentionId: null,
+
+  errors: {},
+  displayedFlights: "outbound",
+  filterableAirlines: null,
+  filterOptionIsOpen: false,
+
+  from: {},
+  to: {},
+  outboundDate: "2020-08-22",
+  inboundDate: "2020-08-26",
+  // Create increment action for each
+  adults: 1,
+  children: 0,
+  infants: 0
+};
+
+function storage(state = initialState, { type, payload }) {
+  switch (type) {
+    case ADD_FLIGHTS:
+      let flights = flights ? flights.concat(payload.flights) : payload.flights;
+      return {
+        ...state,
+        flights: flights
+      };
+    case SET_AIRPORTS:
+      return {
+        ...state,
+        airports: payload.airports
+      };
+    case SET_SEARCH_INTENTION_ID:
+      return {
+        ...state,
+        searchIntentionId: payload.searchIntentionId
+      };
+    default:
+      return state;
+  }
+}
+
+export default createStore(storage, applyMiddleware(thunkMiddleware));
