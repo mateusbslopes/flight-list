d2de854816a0f7088a13f4f5f40f9f99b4ccf0af
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-01-29 17:57:58 -0300

    refactor: #31 Icon

--- src/components/atoms/Icon/index.js
+++ src/components/atoms/Icon/index.js
@@ -1,30 +1,8 @@
 import React from "react";
 import style from "./style";
 
-class Icon extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: props.name,
-      color: props.color,
-      size: props.size,
-      onClick: props.onClick
-    };
-  }
-
-  componentDidMount() {
-    this.setState(this.state);
-  }
-
-  render() {
-    return (
-      <i
-        className={this.state.name}
-        css={() => style(this.state.color, this.state.size)}
-        onClick={this.state.onClick}
-      />
-    );
-  }
+export default function Icon({ name, color, size, onClick }) {
+  return (
+    <i className={name} css={() => style(color, size)} onClick={onClick} />
+  );
 }
-
-export default Icon;
