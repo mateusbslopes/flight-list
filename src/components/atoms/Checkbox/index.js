89dc1c43a79f254978706692f60c76ec140ea8b5
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-01-29 17:54:20 -0300

    refactor: #31 Checkbox

--- src/components/atoms/Checkbox/index.js
+++ src/components/atoms/Checkbox/index.js
@@ -3,25 +3,19 @@ import Icon from "../Icon";
 import Text from "../Text";
 import style from "./style";
 
-class Checkbox extends React.Component {
-  render() {
-    const { toggle, label, checked, id } = this.props;
-
-    return (
-      <div css={style}>
-        <div className="checkbox" onClick={() => toggle(id)}>
-          {checked && (
-            <Icon name="icon-check" color="rgb(26,188,156)" size="small" />
-          )}
-        </div>
-        <div className="checkbox-label">
-          <Text weight="light" size="medium-small">
-            {label}
-          </Text>
-        </div>
+export default function Checkbox({ toggle, label, checked, id }) {
+  return (
+    <div css={style}>
+      <div className="checkbox" onClick={() => toggle(id)}>
+        {checked && (
+          <Icon name="icon-check" color="rgb(26,188,156)" size="small" />
+        )}
       </div>
-    );
-  }
+      <div className="checkbox-label">
+        <Text weight="light" size="medium-small">
+          {label}
+        </Text>
+      </div>
+    </div>
+  );
 }
-
-export default Checkbox;
