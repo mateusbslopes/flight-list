54fae4052ca60ae6c7e0eb0decb2a51e88350152
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-01-29 19:06:07 -0300

    refactor: #31 Card

--- src/components/molecules/Card/index.js
+++ src/components/molecules/Card/index.js
@@ -1,20 +1,14 @@
 import React from "react";
 import style from "./style";
 
-class Card extends React.Component {
-  render() {
-    const { margin, header, body } = this.props;
+export default function Card({ margin, header, body }) {
+  return (
+    <div css={() => style(margin)}>
+      <div className="content">
+        {header && <div className="header">{header}</div>}
 
-    return (
-      <div css={() => style(margin)}>
-        <div className="content">
-          {header && <div className="header">{header}</div>}
-
-          {body && <div className="card-body">{body}</div>}
-        </div>
+        {body && <div className="card-body">{body}</div>}
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default Card;
