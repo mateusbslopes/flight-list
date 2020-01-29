fa5e3fcf57477ff3b9a3e873ef9eaf8774518c3a
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-01-29 17:52:26 -0300

    refactor: #31 Button

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,38 +1,19 @@
 import React from "react";
 import style from "./style";
 
-class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      children: props.children,
-      borderColor: props.borderColor,
-      backgroundColor: props.backgroundColor,
-      onClick: props.onClick,
-      borderBottomColor: props.borderBottomColor
-    };
-  }
-
-  componentDidMount() {
-    this.setState(this.state);
-  }
-
-  render() {
-    return (
-      <div
-        onClick={this.state.onClick}
-        css={() =>
-          style(
-            this.state.borderColor,
-            this.state.backgroundColor,
-            this.state.borderBottomColor
-          )
-        }
-      >
-        {this.state.children}
-      </div>
-    );
-  }
+export default function Button({
+  onClick,
+  borderColor,
+  backgroundColor,
+  borderBottomColor,
+  children
+}) {
+  return (
+    <div
+      onClick={onClick}
+      css={() => style(borderColor, backgroundColor, borderBottomColor)}
+    >
+      {children}
+    </div>
+  );
 }
-
-export default Button;
