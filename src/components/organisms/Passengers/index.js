1458f032564f47c6a4284ee5e1788704aea225df
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-01-29 18:23:26 -0300

    refactor: #31 Passengers

--- src/components/organisms/Passengers/index.js
+++ src/components/organisms/Passengers/index.js
@@ -12,30 +12,26 @@ export default function Passengers({
 }) {
   return (
     <div css={style}>
-      {this.state.optionsIsVisible && (
-        <div className="options col-sm-6">
-          <div className="options-item">
-            <Spinner
-              label="Adultos"
-              value={adults}
-              onChange={onChangeAdults}
-              min={1}
-            />
-          </div>
-          <div className="options-item">
-            <Spinner
-              label="Criancas"
-              value={children}
-              onChange={onChangeChildren}
-            />
-          </div>
-          <div className="options-item">
-            <Spinner label="Bebe" value={infants} onChange={onChangeInfants} />
-          </div>
+      <div className="options col-sm-6">
+        <div className="options-item">
+          <Spinner
+            label="Adultos"
+            value={adults}
+            onChange={onChangeAdults}
+            min={1}
+          />
         </div>
-      )}
+        <div className="options-item">
+          <Spinner
+            label="Criancas"
+            value={children}
+            onChange={onChangeChildren}
+          />
+        </div>
+        <div className="options-item">
+          <Spinner label="Bebe" value={infants} onChange={onChangeInfants} />
+        </div>
+      </div>
     </div>
   );
 }
-
-export default Passengers;
