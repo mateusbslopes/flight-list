9e52b5b695365d69b4baa1252945b861812f8ca2
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-01-29 19:22:52 -0300

    refactor: #31 Calendar

--- src/components/molecules/Calendar/index.js
+++ src/components/molecules/Calendar/index.js
@@ -2,30 +2,25 @@ import React from "react";
 import style from "./style";
 import Icon from "../../atoms/Icon";
 
-class Calendar extends React.Component {
-  render() {
-    const { label, value, handleChange } = this.props;
-    return (
-      <div css={style}>
-        <div>
-          <div className="calendar-label">{label}</div>
-          <div className="col-sm-16">
-            <input
-              className="calendar-input col-sm-8"
-              type="text"
-              value={value}
-              onChange={handleChange}
-            />
-            <Icon
-              color="rgb(26, 188, 156)"
-              size="big"
-              name="max-icon-action-calendar"
-            />
-          </div>
+export default function Calendar({ label, value, handleChange }) {
+  return (
+    <div css={style}>
+      <div>
+        <div className="calendar-label">{label}</div>
+        <div className="col-sm-16">
+          <input
+            className="calendar-input col-sm-8"
+            type="text"
+            value={value}
+            onChange={handleChange}
+          />
+          <Icon
+            color="rgb(26, 188, 156)"
+            size="big"
+            name="max-icon-action-calendar"
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default Calendar;
