99e4820fe5ed86f91fb0c373423836afad8b7e66
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-01-29 19:07:14 -0300

    refactor: #31 FlightColumn

--- src/components/molecules/FlightColumn/index.js
+++ src/components/molecules/FlightColumn/index.js
@@ -2,27 +2,11 @@ import React from "react";
 import style from "./style";
 import Text from "../../atoms/Text/index";
 
-class FlightColumn extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: props.text,
-      info: props.info
-    };
-  }
-
-  componentDidMount() {
-    this.setState(this.state);
-  }
-
-  render() {
-    return (
-      <div css={style}>
-        <Text>{this.state.text}</Text>
-        <Text weight="light">{this.state.info}</Text>
-      </div>
-    );
-  }
+export default function FlightColumn({ text, info }) {
+  return (
+    <div css={style}>
+      <Text>{text}</Text>
+      <Text weight="light">{info}</Text>
+    </div>
+  );
 }
-
-export default FlightColumn;
