35d141273c358168a76df0c673483e570fb8878f
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-01-29 17:54:56 -0300

    refactor: #31 HeaderTitle

--- src/components/atoms/HeaderTitle/index.js
+++ src/components/atoms/HeaderTitle/index.js
@@ -3,19 +3,15 @@ import style from "./style";
 import Icon from "../../atoms/Icon";
 import Text from "../../atoms/Text";
 
-class HeaderTitle extends React.Component {
-  render() {
-    return (
-      <div css={style} className="row">
-        <Icon
-          name="icon-max-navigation-menu"
-          size="big"
-          color="rgb(26, 188, 156)"
-        />
-        <Text size="big">Teste Front</Text>
-      </div>
-    );
-  }
+export default function HeaderTitle() {
+  return (
+    <div css={style} className="row">
+      <Icon
+        name="icon-max-navigation-menu"
+        size="big"
+        color="rgb(26, 188, 156)"
+      />
+      <Text size="big">Teste Front</Text>
+    </div>
+  );
 }
-
-export default HeaderTitle;
