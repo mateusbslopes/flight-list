91cde3ed1c4780b41a2211678a51633d88772dc8
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-02-03 13:50:43 -0300

    feat: #10 Filter flights by outboundHour too

--- src/components/organisms/FlightList/index.js
+++ src/components/organisms/FlightList/index.js
@@ -33,13 +33,35 @@ function FlightList({ flights }) {
   );
 }
 
-function getFlights(flights, flightsToDisplay, airlines) {
+// TODO Refactor filter
+function filterByTime(flight, time) {
+  let hourToCompare = new Date(flight.departureDate);
+
+  switch (time) {
+    case "morning":
+      return hourToCompare.getHours() >= 6 && hourToCompare.getHours() <= 11;
+    case "afternoon":
+      return hourToCompare.getHours() >= 12 && hourToCompare.getHours() <= 17;
+    case "night":
+      return hourToCompare.getHours() >= 18 && hourToCompare.getHours() <= 23;
+    case "dawn":
+      return hourToCompare.getHours() >= 0 && hourToCompare.getHours() <= 5;
+  }
+}
+
+function getFlights(flights, flightsToDisplay, airlines, filter) {
   let flightsToReturn = [];
   flightsToReturn = flights[flightsToDisplay].filter(
     flight =>
       !!airlines.some(
         airline => airline.label === flight.airline && airline.checked
-      )
+      ) &&
+      ((filter.outboundHour.morning.checked &&
+        filterByTime(flight, "morning")) ||
+        (filter.outboundHour.afternoon.checked &&
+          filterByTime(flight, "afternoon")) ||
+        (filter.outboundHour.night.checked && filterByTime(flight, "night")) ||
+        (filter.outboundHour.dawn.checked && filterByTime(flight, "dawn")))
   );
   flightsToReturn = flightsToReturn.sort((flightA, flightB) => {
     return flightA.pricing[flightA.pricing.bestPriceAt].saleTotal <
@@ -51,7 +73,12 @@ function getFlights(flights, flightsToDisplay, airlines) {
 }
 
 const mapStateToProps = state => ({
-  flights: getFlights(state.flights, state.flights.displayed, state.airlines)
+  flights: getFlights(
+    state.flights,
+    state.flights.displayed,
+    state.airlines,
+    state.filter
+  )
 });
 
 export default connect(mapStateToProps)(FlightList);
