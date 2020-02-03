319719841c60baa2823abc74cafc87721ebaa361
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-02-03 16:51:39 -0300

    feat: Make easier to toggle checkbox by clicking on checkbox label eithet

--- src/components/atoms/Checkbox/index.js
+++ src/components/atoms/Checkbox/index.js
@@ -5,8 +5,8 @@ import style from "./style";
 
 export default function Checkbox({ toggle, label, checked, id }) {
   return (
-    <div css={theme => style(theme)}>
-      <div className="checkbox" onClick={() => toggle(id)}>
+    <div css={theme => style(theme)} onClick={() => toggle(id)}>
+      <div className="checkbox">
         {checked && <Icon name="icon-check" size={200} />}
       </div>
       <div className="checkbox-label">
