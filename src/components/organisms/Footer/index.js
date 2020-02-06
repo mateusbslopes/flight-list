251d5de00a79214a9545facc830fcd3f74b2318b
Mateus Lopes <mateuslopes@ENG-2665.local>
2020-02-06 16:33:22 -0300

    feat: #52

--- src/components/organisms/Footer/index.js
+++ src/components/organisms/Footer/index.js
@@ -4,18 +4,28 @@ import { connect } from "react-redux";
 import { openFilter as openFilterAction } from "../../../actions";
 import style from "./style";
 
-function Footer({ openFilter }) {
+function Footer({ openFilter, flights }) {
+  let hasFlightsToFilter =
+    flights.outbound.length > 0 || flights.inbound.length > 0;
+
   return (
     <div css={theme => style(theme)}>
-      <div className="action action-first" onClick={openFilter}>
+      <div
+        className="action action-first"
+        onClick={() => hasFlightsToFilter && openFilter()}
+      >
         <FormattedMessage id="filterFlights" />
       </div>
     </div>
   );
 }
 
+const mapStateToProps = state => ({
+  flights: state.flights
+});
+
 const mapDispatchToProps = {
   openFilter: () => openFilterAction()
 };
 
-export default connect(null, mapDispatchToProps)(Footer);
+export default connect(mapStateToProps, mapDispatchToProps)(Footer);
